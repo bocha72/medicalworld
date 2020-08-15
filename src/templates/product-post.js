9442303500b39ab29ddb21f56e112d003058667e
Alejandro Pérez <alejandro@iurco.com>
2020-08-15 17:11:49 -0300

    second blog ok

---
+++ src/templates/product-post.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { kebabCase } from 'lodash'
+import { Helmet } from 'react-helmet'
+import { graphql, Link } from 'gatsby'
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
+
+export const ProductPostTemplate = ({
+  content,
+  contentComponent,
+  description,
+  tags,
+  title,
+  helmet,
+}) => {
+  const PostContent = contentComponent || Content
+
+  return (
+    <section className="section">
+      {helmet || ''}
+      <div className="container content">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+              {title}
+            </h1>
+            <p>{description}</p>
+            <PostContent content={content} />
+            {tags && tags.length ? (
+              <div style={{ marginTop: `4rem` }}>
+                <h4>Tags</h4>
+                <ul className="taglist">
+                  {tags.map((tag) => (
+                    <li key={tag + `tag`}>
+                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+ProductPostTemplate.propTypes = {
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  description: PropTypes.string,
+  title: PropTypes.string,
+  helmet: PropTypes.object,
+}
+
+const ProductPost = ({ data }) => {
+  const { markdownRemark: post2 } = data
+
+  return (
+    <Layout>
+      <ProductPostTemplate
+        content={post2.html}
+        contentComponent={HTMLContent}
+        description={post2.frontmatter.description}
+        helmet={
+          <Helmet titleTemplate="%s | Product">
+            <title>{`${post2.frontmatter.title}`}</title>
+            <meta
+              name="description"
+              content={`${post2.frontmatter.description}`}
+            />
+          </Helmet>
+        }
+        tags={post2.frontmatter.tags}
+        title={post2.frontmatter.title}
+      />
+    </Layout>
+  )
+}
+
+ProductPost.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
+}
+
+export default ProductPost
+
+export const pageQuery = graphql`
+  query ProductPostByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        description
+        tags
+      }
+    }
+  }
+`
