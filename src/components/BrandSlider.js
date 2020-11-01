b0758d16cf43595bc7cf70e98105b96691673c7d
Alejandro Pérez <alejandro@iurco.com>
2020-11-01 19:54:31 -0300

    add images from brandslider

--- src/components/BrandSlider.js
+++ src/components/BrandSlider.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
 import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css'
+import brand01 from '../../static/img/brands-logos/brand01.png'
+import brand02 from '../../static/img/brands-logos/brand02.png'
+import brand03 from '../../static/img/brands-logos/brand03.png'
+import brand04 from '../../static/img/brands-logos/brand04.png'
+import brand05 from '../../static/img/brands-logos/brand05.png'
+import brand06 from '../../static/img/brands-logos/brand06.png'
+import brand07 from '../../static/img/brands-logos/brand07.png'
+import brand08 from '../../static/img/brands-logos/brand08.png'
+import brand09 from '../../static/img/brands-logos/brand09.png'
+import brand10 from '../../static/img/brands-logos/brand10.png'
 
 const responsive = {
     superLargeDesktop: {
@@ -9,7 +19,7 @@ const responsive = {
     },
     desktop: {
         breakpoint: { max: 3000, min: 1024 },
-        items: 8,
+        items: 5,
     },
     tablet: {
         breakpoint: { max: 1024, min: 464 },
@@ -27,17 +37,22 @@ class BrandSlider extends Component {
         return (
             <Carousel 
                 responsive={responsive}
-                autoPlay={false}
-                autoPlaySpeed={2000}
+                autoPlay={true}
+                autoPlaySpeed={2500}
                 removeArrowOnDeviceType={["desktop", "tablet", "mobile"]}
                 infinite={true}
                 customTransition="transform 400ms ease-in-out 0s"
             >
-                <div>Brand logo 1</div>
-                <div>Brand logo 2</div>
-                <div>Brand logo 3</div>
-                <div>Brand logo 4</div>
-                <div>Brand logo 5</div>
+                <div><img src={brand01} alt="" className="brand-logo"/></div>
+                <div><img src={brand02} alt="" className="brand-logo"/></div>
+                <div><img src={brand03} alt="" className="brand-logo"/></div>
+                <div><img src={brand04} alt="" className="brand-logo"/></div>
+                <div><img src={brand05} alt="" className="brand-logo"/></div>
+                <div><img src={brand06} alt="" className="brand-logo"/></div>
+                <div><img src={brand07} alt="" className="brand-logo"/></div>
+                <div><img src={brand08} alt="" className="brand-logo"/></div>
+                <div><img src={brand09} alt="" className="brand-logo"/></div>
+                <div><img src={brand10} alt="" className="brand-logo"/></div>
             </Carousel>
         )
     }
